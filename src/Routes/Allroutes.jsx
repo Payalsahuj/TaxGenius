11408562f8eb97984cfc1bafedfe0af72a19dc5b
prajit96 <santraprajit@gmail.com>
2023-05-05 20:28:46 +0530

    calculators task running

--- src/Routes/Allroutes.jsx
+++ src/Routes/Allroutes.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../Pages/Home"
+import { CalculatorPage } from "../Pages/CalculatorPage"
+import { SalarytaxPage } from "../Pages/SalarytaxPage"
 
 
 function Allroutes(){
     return <div>
         <Routes>
             <Route path="/" element={<Home/>}/>
-            
+            <Route path="/calculators" element={<CalculatorPage/>}/>
+            <Route path="/salarytaxpage" element={<SalarytaxPage/>}/>
         </Routes>
     </div>
 }
