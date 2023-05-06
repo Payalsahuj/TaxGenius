f68d963a225fdb58f568af1b9dde51c4203244b0
prajit96 <santraprajit@gmail.com>
2023-05-06 20:56:16 +0530

    travel pages

--- src/Routes/Allroutes.jsx
+++ src/Routes/Allroutes.jsx
@@ -5,6 +5,7 @@ import { CalculatorPage } from "../Pages/CalculatorPage"
 import { SalarytaxPage } from "../Pages/SalarytaxPage"
 
 import  LogTool  from "../Pages/LogTool"
+import TravelDeductionPage from "../Pages/TaravelDeductionPage"
 
 
 
@@ -15,7 +16,7 @@ function Allroutes(){
 
             <Route path="/calculators" element={<CalculatorPage/>}/>
             <Route path="/salarytaxpage" element={<SalarytaxPage/>}/>
-
+            <Route path="/traveldeductionpage" element={<TravelDeductionPage/>}/>
             <Route path="/tools" element={<LogTool />}/>
 
         </Routes>
