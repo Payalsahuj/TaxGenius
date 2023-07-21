ef2abfea8cb9b80a6e1223ee0d130048b2c379cc
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-07-21 18:29:04 +0530

    Auth

--- src/Redux/logindata/action.jsx
+++ src/Redux/logindata/action.jsx
@@ -2,11 +2,11 @@ import axios from "axios"
 import { POST_DATA, POST_details } from "./actiontype"
 
 export const handlepost=(data)=>(dispatch)=>{
-   return axios.post(`https://kjyt12.sse.codesandbox.io/login`,data).then((res)=>dispatch({type:POST_DATA,payload:res.data}))
+   return axios.post(`https://taxtim.onrender.com/login`,data).then((res)=>dispatch({type:POST_DATA,payload:res.data}))
 }
 
 
 
 export const handlepostdetails=(data)=>(dispatch)=>{
-    return axios.post(`https://kjyt12.sse.codesandbox.io/userdetail`,data).then((res)=>dispatch({type:POST_details,payload:res.data}))
+    return axios.post(`https://taxtim.onrender.com/userdetail`,data).then((res)=>dispatch({type:POST_details,payload:res.data}))
  }
